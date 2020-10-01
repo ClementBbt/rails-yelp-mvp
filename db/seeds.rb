eb17688272972b5a5bbf17aad5ca11878ce7ee02
Clement Bibolet <clementbibolet@gmail.com>
2020-10-01 20:33:25 +0200

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning data base"
+Restaurant.destroy_all
+
+
+puts "creating restaurant"
+bleu_resto = { name: "bleu resto", address: "11 rue bleue", category: "chinese" }
+rouge_resto = { name: "rouge resto", address: "11 rue rouge", category: "italian" }
+yellow_resto = { name: "yellow resto", address: "11 rue jaune", category: "japanese" }
+green_resto = { name: "green resto", address: "11 rue verte", category: "french" }
+white_resto = { name: "white resto", address: "11 rue white", category: "belgian" }
+
+[bleu_resto, rouge_resto, yellow_resto, green_resto, white_resto].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "finished"
